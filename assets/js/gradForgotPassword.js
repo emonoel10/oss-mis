246dd59117df6954a40180c2507b91811699ef74
Noel Calonia <emonoel10@gmail.com>
2018-04-21 20:43:42 +0800

    Codebase > Graduate Forgot Password module tweaks: change module controller in js file

--- assets/js/gradForgotPassword.js
+++ assets/js/gradForgotPassword.js
@@ -9,7 +9,7 @@ var forgotPassword = (function () {
             $("form#resetPasswordForm button#resetPasswordSubmit").empty().html('<div class="sk-fading-circle"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>');
 
             $.ajax({
-                url: 'socioForgotPasswordController.php',
+                url: 'gradForgotPasswordController.php',
                 type: 'POST',
                 data: account,
                 dataType: 'json',
@@ -39,7 +39,7 @@ var forgotPassword = (function () {
             $("form#confirmResetPasswordForm button#confirmResetPasswordSubmit").empty().html('<div class="sk-fading-circle"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>');
 
             $.ajax({
-                url: 'socioForgotPasswordController.php',
+                url: 'gradForgotPasswordController.php',
                 type: 'POST',
                 data: account,
                 dataType: 'json',
